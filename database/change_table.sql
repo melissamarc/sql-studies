d7039daf3ad6194661a49ec3c7fcbd533d3a2031
Melissa Marcielo <melissamarcielo@192.168.1.102>
2026-08-31 19:53:33 -0300

    alterando tabelas, tamanho de columns e adicionando primary key apos ja ter feito a tabela

---
+++ database/change_table.sql
@@ -0,0 +1,25 @@
+use cadastro;
+
+desc pessoas;
+
+alter table pessoas drop column codigo;
+alter table pessoas drop column profissao;
+
+alter table pessoas add column profissao varchar(10);
+alter table pessoas modify column profissao varchar(20);
+
+alter table pessoas add column codigo int first;
+alter table pessoas rename to clientes;
+
+describe clientes;
+
+create table if not exists cursos(
+nome varchar(30) not null unique,
+descricao text,
+carga int unsigned,
+totaulas int unsigned,
+ano year default '2026'
+)default charset=utf8;
+
+alter table cursos add column idcurso int first;
+alter table cursos add primary key(idcurso);
